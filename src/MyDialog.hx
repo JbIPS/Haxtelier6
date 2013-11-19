b9f226519aeefafb0517e39a55567c4e33328779
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-11-19 20:16:10 +0100

    Patterns

--- src/MyDialog.hx
+++ src/MyDialog.hx
@@ -1,5 +1,6 @@
 package ;
 
+import flash.text.TextFormat;
 import flash.events.MouseEvent;
 import flash.display.Shape;
 import flash.Lib;
@@ -13,6 +14,7 @@ class MyDialog extends Sprite{
 	private var nextButton: SimpleButton;
 	private var texts: Array<String>;
 	private var textIndex: Int = 0;
+	private var pattern: Pattern;
 
 	/**
 	* Méthode d'entrée du programme
@@ -28,19 +30,28 @@ class MyDialog extends Sprite{
 		// Appel du constructeur de Sprite
 		super();
 		texts = ["Salut", "tu", "vas", "bien ?"];
+		var newPattern = {name: "Pattern2", texts: ["Bien", "et toi ?"], next: null};
+		pattern = {name: "Pattern", texts: texts, next: newPattern};
 
 		textField = new TextField();
 
 		//Dessine le bouton
+		var buttonContent = new Sprite();
 		var shape = new Shape();
 		shape.graphics.beginFill(0xFF0000);
 		shape.graphics.drawRect(0, 0, 100, 50);
 		shape.graphics.endFill();
+		var buttonText = new TextField();
+		buttonText.defaultTextFormat = new TextFormat("_sans", 10, 0xFFFFFF);
+		buttonText.text = "Suivant";
+		buttonContent.addChild(shape);
+		buttonContent.addChild(buttonText);
 		// 3 états du bouton + zone de clic
-		nextButton = new SimpleButton(shape, shape, shape, shape);
+		nextButton = new SimpleButton(buttonContent, buttonContent, buttonContent, buttonContent);
 		nextButton.x = 500;
 		nextButton.y = 350;
-		nextButton.addEventListener(MouseEvent.CLICK, displayNextText);
+		nextButton.addEventListener(MouseEvent.CLICK, onButtonClick);
+		textField.addEventListener(MouseEvent.CLICK, onButtonClick);
 
 		display();
 	}
@@ -53,19 +64,37 @@ class MyDialog extends Sprite{
 		Lib.current.addChild(this);
 	}
 
-	private function displayNextText(e:MouseEvent):Void
+	private function onButtonClick(e: MouseEvent):Void
+	{
+		displayNextText();
+	}
+
+	private function displayNextText():Void
 	{
 		// Vérifier l'indice pour ne pas sortir du tableau
-		if(textIndex < texts.length){
-			setText(texts[textIndex]);
-			textIndex++;
+		if(textIndex < pattern.texts.length){
+			if(pattern != null){
+				setText(pattern.texts[textIndex]);
+				textIndex++;
+			}
+		}
+		else{
+			pattern = pattern.next;
+			textIndex = 0;
+			displayNextText();
 		}
 		// En une ligne
-		// setText(texts[++textIndex]);
+		// setText(texts[textIndex++]);
 	}
 
 	private function setText(text:String):Void
 	{
 		textField.text = text;
 	}
+}
+
+typedef Pattern = {
+	var name: String;
+	var texts: Array<String>;
+	var next: Pattern;
 }
