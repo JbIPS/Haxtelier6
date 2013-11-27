09c46821f10c3f690a99aa2474820fdb145720b6
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-11-27 12:03:43 +0100

    Fin patterns

--- src/MyDialog.hx
+++ src/MyDialog.hx
@@ -13,8 +13,11 @@ class MyDialog extends Sprite{
 
 	private var textField: TextField;
 	private var nextButton: SimpleButton;
+	private var choiceButton1: SimpleButton;
+	private var choiceButton2: SimpleButton;
 	private var textIndex: Int = 0;
 	private var currentPattern: Pattern;
+	private var patterns: Map<String, Pattern>;
 
 	/**
 	* Méthode d'entrée du programme
@@ -29,10 +32,18 @@ class MyDialog extends Sprite{
 	{
 		// Appel du constructeur de Sprite
 		super();
+		patterns = new Map<String, Pattern>();
 
 		// Création des patterns
-		var pattern1 = {name: "Pattern2", texts: ["Bien", "et toi ?"], next: null};
-		var pattern0 = {name: "Pattern", texts: ["Salut", "tu", "vas", "bien ?"], next: pattern1};
+		var fin: Pattern = {name: "Fin", texts: ["GAME OVER"]};
+		var pattern1: Pattern = {name: "Pattern1", texts: ["Bien", "et toi ?"], next: fin};
+		var pattern0: Pattern = {name: "Pattern0", texts: ["Salut", "tu", "vas", "bien ?"]};
+		var pattern2: Pattern = {name: "Pattern2", texts: ["Bof, pas trop", "je suis bloqué sur un bug"], next: fin};
+		// Remplissage de la map
+		patterns.set(pattern0.name, pattern0);
+		patterns.set(pattern1.name, pattern1);
+		patterns.set(pattern2.name, pattern2);
+
 		currentPattern = pattern0;
 
 		textField = new TextField();
@@ -40,7 +51,7 @@ class MyDialog extends Sprite{
 		//Dessine le bouton
 		var buttonContent = new Sprite();
 		var shape = new Shape();
-		shape.graphics.beginFill(0xFF0000);
+		shape.graphics.beginFill(0x0000FF);
 		shape.graphics.drawRect(0, 0, 100, 50);
 		shape.graphics.endFill();
 		var buttonText = new TextField();
@@ -54,10 +65,52 @@ class MyDialog extends Sprite{
 		buttonText.y = shape.height /2 - buttonText.height / 2;
 		// 3 états du bouton + zone de clic
 		nextButton = new SimpleButton(buttonContent, buttonContent, buttonContent, buttonContent);
+		nextButton.useHandCursor = true;
 		nextButton.x = 500;
 		nextButton.y = 350;
 		nextButton.addEventListener(MouseEvent.CLICK, onButtonClick);
-		textField.addEventListener(MouseEvent.CLICK, onButtonClick);
+
+		//Dessine les boutons de choix
+		var buttonContent = new Sprite();
+		var shape = new Shape();
+		shape.graphics.beginFill(0xFF0000);
+		shape.graphics.drawRect(0, 0, 100, 50);
+		shape.graphics.endFill();
+		var buttonText = new TextField();
+		buttonText.autoSize = TextFieldAutoSize.CENTER;
+		buttonText.defaultTextFormat = new TextFormat("_sans", 10, 0xFFFFFF);
+		buttonText.text = "Non";
+		buttonContent.addChild(shape);
+		buttonContent.addChild(buttonText);
+		// Centre le texte
+		buttonText.x = shape.width / 2 - buttonText.width / 2;
+		buttonText.y = shape.height /2 - buttonText.height / 2;
+		// 3 états du bouton + zone de clic
+		choiceButton1 = new SimpleButton(buttonContent, buttonContent, buttonContent, buttonContent);
+		choiceButton1.name = "non";
+		choiceButton1.y = 350;
+		choiceButton1.addEventListener(MouseEvent.CLICK, onButtonClick);
+
+		var buttonContent = new Sprite();
+		var shape = new Shape();
+		shape.graphics.beginFill(0x00FF00);
+		shape.graphics.drawRect(0, 0, 100, 50);
+		shape.graphics.endFill();
+		var buttonText = new TextField();
+		buttonText.autoSize = TextFieldAutoSize.CENTER;
+		buttonText.defaultTextFormat = new TextFormat("_sans", 10, 0xFFFFFF);
+		buttonText.text = "Oui";
+		buttonContent.addChild(shape);
+		buttonContent.addChild(buttonText);
+		// Centre le texte
+		buttonText.x = shape.width / 2 - buttonText.width / 2;
+		buttonText.y = shape.height /2 - buttonText.height / 2;
+		// 3 états du bouton + zone de clic
+		choiceButton2 = new SimpleButton(buttonContent, buttonContent, buttonContent, buttonContent);
+		choiceButton2.name = "oui";
+		choiceButton2.x = 500;
+		choiceButton2.y = 350;
+		choiceButton2.addEventListener(MouseEvent.CLICK, onButtonClick);
 
 		display();
 	}
@@ -72,26 +125,51 @@ class MyDialog extends Sprite{
 
 	private function onButtonClick(e: MouseEvent):Void
 	{
-		displayNextText();
+		var name: String = null;
+		if(Std.is(e.currentTarget, SimpleButton))
+			name = cast(e.currentTarget, SimpleButton).name;
+		displayNextText(name);
 	}
 
-	private function displayNextText():Void
+	private function displayNextText(?choice: String):Void
 	{
-		// Vérifier l'indice pour ne pas sortir du tableau et que currentPattern n'est pas null
-		if(currentPattern != null && textIndex < currentPattern.texts.length){
-			setText(currentPattern.texts[textIndex]);
-			textIndex++;
-			// En une ligne
-			// setText(texts[textIndex++]);
-		}
-		else if(currentPattern != null){
-			// On pourrait aussi désactiver le bouton quand currentPattern est null
-			// nextButton.enabled = false;
-			// ou carrément lui supprimer son listener
-			// nextButton.removeEventListener(MouseEvent.CLICK, onButtonClick);
-			currentPattern = currentPattern.next;
-			textIndex = 0;
-			displayNextText();
+		// On pourrait aussi désactiver le bouton quand currentPattern est null
+		// nextButton.enabled = false;
+		// ou carrément lui supprimer son listener
+		// nextButton.removeEventListener(MouseEvent.CLICK, onButtonClick);
+		if(currentPattern != null){
+			// Vérifier l'indice pour ne pas sortir du tableau
+			if(textIndex < currentPattern.texts.length){
+				setText(currentPattern.texts[textIndex]);
+				textIndex++;
+				// En une ligne
+				// setText(texts[textIndex++]);
+
+				// Détection de fin de pattern
+				if(textIndex == currentPattern.texts.length && currentPattern.next == null && currentPattern.name != "Fin"){
+					// Ajout du bouton alternatif
+					addChild(choiceButton1);
+					addChild(choiceButton2);
+				}
+			}
+			else if(currentPattern.next != null){
+				currentPattern = currentPattern.next;
+				if(currentPattern.name == "Fin")
+					removeChild(nextButton);
+				textIndex = 0;
+				displayNextText();
+			}
+			else if(choice != null){
+				currentPattern = switch(choice){
+					case "oui" : patterns.get("Pattern1"); // ou patterns["Pattern1"]
+					case "non" : patterns.get("Pattern2");
+					default : throw "Je ne connais pas ce choix";
+				}
+				textIndex = 0;
+				removeChild(choiceButton1);
+				removeChild(choiceButton2);
+				displayNextText();
+			}
 		}
 	}
 
@@ -105,5 +183,5 @@ class MyDialog extends Sprite{
 typedef Pattern = {
 	var name: String;
 	var texts: Array<String>;
-	var next: Pattern;
+	@:optional var next: Pattern;
 }
